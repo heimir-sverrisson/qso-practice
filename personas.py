b9d449796bd3d6f390a6c116d9623b15e558dfc2
Heimir Thor Sverrisson <heimir.sverrisson@gmail.com>
2023-12-10 15:26:34 -0700

    Added random personas with call_sign, name and qth.

---
+++ personas.py
@@ -0,0 +1,420 @@
+import random
+import send_code as sc
+import string
+
+
+def _make_call_sign(country):
+    prefix_length = random.choice([1, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3])
+    postfix_length = random.choice([1, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3])
+    call_sign = ""
+    if country.lower() == "usa":
+        prefix_length = random.choice([1, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3])
+        postfix_length = random.choice([1, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3])
+        first_letter = random.choice(["k", "k", "w", "w" "n", "n", "a"])
+        digit = random.choice(string.digits)
+        call_sign += first_letter
+        for i in range(1, prefix_length):
+            char = random.choice(string.ascii_lowercase)
+            call_sign += char
+        if first_letter == "a" and prefix_length > 1:
+            cs = list(call_sign)
+            cs[1] = random.choice(string.ascii_lowercase[:12])  # only a-l
+            call_sign = "".join(cs)
+        call_sign += digit
+        for i in range(postfix_length):
+            char = random.choice(string.ascii_lowercase)
+            call_sign += char
+    if country.lower() == "iceland":
+        postfix_length = random.choice([1, 2, 2, 2, 2, 2, 2, 3])
+        call_sign = "tf"
+        digit = random.choice(
+            ["1", "2", "3", "3", "3", "3", "3", "3", "3", "4", "5", "6", "7", "8", "9"]
+        )
+        call_sign += digit
+        for i in range(postfix_length):
+            char = random.choice(string.ascii_lowercase)
+            call_sign += char
+        if postfix_length == 3:
+            cs = list(call_sign)
+            cs[-1] = random.choice(["n", "t", "t"])
+            call_sign = "".join(cs)
+    return call_sign
+
+
+def _make_name(country):
+    usa_names = [
+        "Abel",
+        "Adam",
+        "Adan",
+        "Alan",
+        "Aldo",
+        "Alec",
+        "Amir",
+        "Amos",
+        "Aram",
+        "Ari",
+        "Arlo",
+        "Arndt",
+        "Asa",
+        "Axel",
+        "Beau",
+        "Blaise",
+        "Blaine",
+        "Boyd",
+        "Bram",
+        "Brett",
+        "Brent",
+        "Bryn",
+        "Cai",
+        "Cain",
+        "Carl",
+        "Cian",
+        "Clark",
+        "Claus",
+        "Cole",
+        "Colm",
+        "Craig",
+        "Dean",
+        "Dion",
+        "Eli",
+        "Elis",
+        "Enzo",
+        "Eric",
+        "Ernst",
+        "Esau",
+        "Ezio",
+        "Ezra",
+        "Euan",
+        "Evan",
+        "Fulk",
+        "Finn",
+        "Flynn",
+        "Frank",
+        "Franz",
+        "Gael",
+        "Gene",
+        "Giles",
+        "Glenn",
+        "Grant",
+        "Greer",
+        "Greig",
+        "Grey",
+        "Guy",
+        "Hans",
+        "Heath",
+        "Hugh",
+        "Hugo",
+        "Ian",
+        "Ioan",
+        "Ivan",
+        "Ivo",
+        "Ivor",
+        "Iwan",
+        "Jago",
+        "Jair",
+        "Joah",
+        "Joel",
+        "Jory",
+        "Jude",
+        "Kai",
+        "Keir",
+        "Keith",
+        "Kerr",
+        "Kurt",
+        "Lars",
+        "Leif",
+        "Lee",
+        "Leo",
+        "Leon",
+        "Lev",
+        "Levi",
+        "Lex",
+        "Liam",
+        "Lloyd",
+        "Loic",
+        "Luke",
+        "Luca",
+        "Ludo",
+        "Mac",
+        "Max",
+        "Miles",
+        "Milo",
+        "Neil",
+        "Nico",
+        "Nils",
+        "Noah",
+        "Noam",
+        "Noel",
+        "Otis",
+        "Otto",
+        "Owen",
+        "Rafe",
+        "Ralph",
+        "Reid",
+        "Rex",
+        "Rhett",
+        "Rhys",
+        "Rio",
+        "Ross",
+        "Saul",
+        "Scott",
+        "Sean",
+        "Seth",
+        "Tate",
+        "Teo",
+        "Theo",
+        "Thor",
+        "Todd",
+        "Urs",
+        "Vaughn",
+        "Veit",
+        "Wolfe",
+        "Zeno",
+        "Zeus ",
+    ]
+    icelandic_names = [
+        "Alli",
+        "Aggi",
+        "Alex",
+        "Andres",
+        "Andri",
+        "Anton",
+        "Ari",
+        "Arnar",
+        "Arnor",
+        "Aron",
+        "Atli",
+        "Axel",
+        "Agust",
+        "Armann",
+        "Arni",
+        "Asgeir",
+        "Asi",
+        "Baldur",
+        "Baldvin",
+        "Benni",
+        "Beggi",
+        "Biggi",
+        "Birkir",
+        "Bjarki",
+        "Bjarni",
+        "Bjoggi",
+        "Bjossi",
+        "Bragi",
+        "Binni",
+        "Dadi",
+        "Dagur",
+        "Danni",
+        "David",
+        "Eggert",
+        "Egill",
+        "Einar",
+        "Eirikur",
+        "Elias",
+        "Elvar",
+        "Emil",
+        "Eythor",
+        "Fannar",
+        "Finnur",
+        "Fridrik",
+        "Gardar",
+        "Geir",
+        "Gisli",
+        "Gretar",
+        "Gudjon",
+        "Gulli",
+        "Gummi",
+        "Gudni",
+        "Gunnar",
+        "Gunni",
+        "Gylfi",
+        "Haffi",
+        "Dori",
+        "Hannes",
+        "Haddi",
+        "Haukur",
+        "Hakon",
+        "Heidar",
+        "Heimir",
+        "Helgi",
+        "Hemmi",
+        "Hilmar",
+        "Hjalti",
+        "Hjalli",
+        "Hjortur",
+        "Hlynur",
+        "Hreinn",
+        "Hordur",
+        "Ingi",
+        "Ingo",
+        "Ingvar",
+        "Isak",
+        "Ivar",
+        "Jakob",
+        "Jens",
+        "Johann",
+        "Johannes",
+        "Jon",
+        "Jonas",
+        "Julius",
+        "Karl",
+        "Kari",
+        "Kjartan",
+        "Kolbeinn",
+        "Kristinn",
+        "Kristjan",
+        "Kristofer",
+        "Larus",
+        "Leifur",
+        "Magnus",
+        "Matti",
+        "Oddur",
+        "Oli",
+        "Omar",
+        "Oskar",
+        "Pall",
+        "Palmi",
+        "Petur",
+        "Raggi",
+        "Reynir",
+        "Robert",
+        "Runar",
+        "Fusi",
+        "Siggi",
+        "Jonni",
+        "Sindri",
+        "Skuli",
+        "Smari",
+        "Snorri",
+        "Stefan",
+        "Steini",
+        "Svavar",
+        "Svenni",
+        "Sverrir",
+        "Saemi",
+        "Saevar",
+        "Tommi",
+        "Trausti",
+        "Tryggvi",
+        "Valdi",
+        "Valli",
+        "Valur",
+        "Vidar",
+        "Vignir",
+        "Viktor",
+        "Villi",
+        "Thor",
+        "Thordur",
+        "Thorir",
+        "Throstur",
+        "Orn",
+    ]
+    if country.lower() == "usa":
+        return random.choice(usa_names).lower()
+    elif country.lower() == "iceland":
+        return random.choice(icelandic_names).lower()
+
+
+def _make_location(country):
+    usa_location = [
+        "AL",
+        "KY",
+        "OH",
+        "AK",
+        "LA",
+        "OK",
+        "AZ",
+        "ME",
+        "OR",
+        "AR",
+        "MD",
+        "PA",
+        "AS",
+        "MA",
+        "PR",
+        "CA",
+        "MI",
+        "RI",
+        "CO",
+        "MN",
+        "SC",
+        "CT",
+        "MS",
+        "SD",
+        "DE",
+        "MO",
+        "TN",
+        "DC",
+        "MT",
+        "TX",
+        "FL",
+        "NE",
+        "TT",
+        "GA",
+        "NV",
+        "UT",
+        "GU",
+        "NH",
+        "VT",
+        "HI",
+        "NJ",
+        "VA",
+        "ID",
+        "NM",
+        "VI",
+        "IL",
+        "NY",
+        "WA",
+        "IN",
+        "NC",
+        "WV",
+        "IA",
+        "ND",
+        "WI",
+        "KS",
+        "MP",
+        "WY",
+    ]
+    icelandic_location = [
+        "reykjavik",
+        "reykjavik",
+        "reykjavik",
+        "reykjavik",
+        "reykjavik",
+        "reykjavik",
+        "akureyri",
+        "borgarnes",
+        "selfoss",
+        "hafnarfjordur",
+        "kopavogur",
+        "keflavik",
+        "seydisfjordur",
+    ]
+    if country.lower() == "usa":
+        return random.choice(usa_location)
+    elif country.lower() == "iceland":
+        return random.choice(icelandic_location)
+
+
+def get_persona(country):
+    persona = {}
+    persona = {
+        "call_sign": _make_call_sign(country),
+        "name": _make_name(country),
+        "qth": _make_location(country),
+    }
+    return persona
+
+
+if __name__ == "__main__":
+    import sys
+
+    if len(sys.argv) < 2:
+        print("Usage: python personas.py <country>")
+        sys.exit(1)
+    country = sys.argv[1]
+    if country.lower() not in ["usa", "iceland"]:
+        print("Country must be USA or Iceland")
+        sys.exit(1)
+    for i in range(100):
+        print(get_persona(country))
--- qso.py
+++ qso.py
@@ -1,17 +1,29 @@
 import sys
 import send_code as sc
 import read_code as rc
+import personas as ps
 
 
-def send_cq(call_sign):
+def send_cq(persona):
+    call_sign = persona["call_sign"]
     sc.send_code(f"cq cq cq de {call_sign} {call_sign} k")
 
 
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python qso.py <country>")
+        sys.exit(1)
+    country = sys.argv[1]
+    if country.lower() not in ["usa", "iceland"]:
+        print("Country must be USA or Iceland")
+        sys.exit(1)
+    persona = ps.get_persona(country)
     while True:
-        send_cq("w1aw")
+        send_cq(persona)
         response = rc.read_code()
         print()
-        if "w1aw" in response.lower() and "w1ant" in response.lower():
+        if persona["call_sign"] in response.lower() and "w1ant" in response.lower():
             print("Both call signs correct!")
             break
+        else:
+            print(f"Did not hear {persona['call_sign']} or w1ant in response.")
